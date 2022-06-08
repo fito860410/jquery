ea5f2dc2d8257cd7eca81aa790ec74384ed7ff03
fito860410 <fito860410@gmail.com>
2022-06-08 07:29:54 -0400

    comentar metodo animador

--- src/effects/animatedSelector.js
+++ src/effects/animatedSelector.js
@@ -2,7 +2,7 @@ import jQuery from "../core.js";
 
 import "../selector.js";
 import "../effects.js";
-
+//animador
 jQuery.expr.pseudos.animated = function( elem ) {
 	return jQuery.grep( jQuery.timers, function( fn ) {
 		return elem === fn.elem;
